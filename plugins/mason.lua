dbcad04690417674af998b213447b535aa483e01
brianliu <hong86323@gmail.com>
2023-04-07 01:03:46 +0800

    add basic python ide plugins

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,15 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      -- ensure_installed = { "lua_ls" },
+      ensure_installed = {
+        -- lua
+        "lua_ls",
+        -- python
+        "pylsp",
+        -- docker
+        "dockerls",
+        "docker_compose_language_service",
+      },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
@@ -13,14 +21,23 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      -- ensure_installed = { "prettier", "stylua" },
+      ensure_installed = {
+        -- lua
+        "stylua",
+        -- python
+        "black",
+        "flake8",
+      },
     },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = {
-      -- ensure_installed = { "python" },
+      ensure_installed = {
+        -- python
+        "python",
+      },
     },
   },
 }
